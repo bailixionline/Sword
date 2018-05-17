2bb4d54d2948ec9fc6a3fdb629de124c4c163554
bailixi <bailixionline@hotmail.com>
2018-05-17 19:26:00 +0100

    character move test

---
+++ Swords/Assets/scripts/CharacterController2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+    //for movement
+    public Rigidbody2D rg;
+    public float speed=3;
+
+	// Use this for initialization
+	void Start () {
+        rg = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rg.velocity = new Vector2(moveX * speed, moveY * speed);
+	}
+}
