34e38235dc1a0cb47aac74a473207e380d95da3d
bailixi <bailixionline@hotmail.com>
2018-05-19 21:29:10 +0100

    boots, flip function added

--- Swords/Assets/scripts/CharacterController2D.cs
+++ Swords/Assets/scripts/CharacterController2D.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class CharacterController2D : MonoBehaviour {
 
     //for movement
-    public Rigidbody2D rg;
-    public float speed=3;
+    Rigidbody2D rg;
+    bool facingRight;
+    public float speed;
+    public float bootsTimes;
 
 	// Use this for initialization
 	void Start () {
         rg = gameObject.GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {              
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+        bool boots = Input.GetButton("Fire3");
 
-        rg.velocity = new Vector2(moveX * speed, moveY * speed);
-	}
+        HandleMovement(moveX, moveY,boots);
+        Flip(moveX);
+    }
+
+    private void HandleMovement(float x,float y,bool boots)
+    {
+        rg.velocity = !boots? new Vector2(x * speed, y * speed): new Vector2(x * speed * bootsTimes, y * speed * bootsTimes);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal <0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
 }
+ 
